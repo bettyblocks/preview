06bce98c19410c924d2c2ea118a963e8b987c556
Bas Dirks <bas.dirks@bettyblocks.com>
2019-11-12 10:20:28 +0100

    fix: disable key-order check

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,39 @@
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "prettier/@typescript-eslint",
-    "airbnb",
-    "airbnb/hooks",
-    "plugin:prettier/recommended"
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier/@typescript-eslint',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended'
   ],
-  plugins: ["react", "@typescript-eslint", "prettier"],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   env: {
     browser: true,
     jasmine: true,
     jest: true
   },
   rules: {
-    "prettier/prettier": ["error", { singleQuote: true }],
-    "react/jsx-filename-extension": [1, { extensions: ["tsx"] }],
-    "react/jsx-wrap-multilines": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/jsx-curly-newline": 0,
-    "sort-keys": 1
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/jsx-filename-extension': [1, { extensions: ['tsx'] }],
+    'react/jsx-wrap-multilines': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-curly-newline': 0
   },
   settings: {
     react: {
-      pragma: "React",
-      version: "detect"
+      pragma: 'React',
+      version: 'detect'
     },
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"]
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
       }
     }
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   globals: {
     process: true
   }
